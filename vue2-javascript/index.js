28e8816466fd5d1f549cd24f0447437a8709ebdb
hewx <1825724721@qq.com>
2023-05-05 13:11:10 +0800

    新增vue2-javascript

---
+++ vue2-javascript/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:promise/recommended',
+    'plugin:vue/recommended'
+  ],
+  overrides: [],
+  parserOptions: { ecmaVersion: 'latest' },
+  globals: { // 全局变量
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue', 'scss'], // @/引入问题
+      },
+    },
+  },
+  plugins: ['import', 'promise', 'vue'],
+  rules: {
+    'no-param-reassign': ['error', { ignorePropertyModificationsFor: ['state'] }], // 函数参数可修改白名单
+  },
+};
