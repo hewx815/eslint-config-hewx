ccb57d9185afe3a1d456bd311d6a501585989f38
hewx <1825724721@qq.com>
2023-05-14 17:35:50 +0800

    1.0.0

--- node-typescript/index.js
+++ node-typescript/index.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:node/recommended',
     'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:promise/recommended',
     'plugin:@typescript-eslint/recommended',
   ],
@@ -17,5 +18,13 @@ module.exports = {
   },
   plugins: ['import', 'promise', 'node','@typescript-eslint'],
   rules: {
+    "node/no-unsupported-features/es-syntax": [0],
+    "node/no-missing-import": [0]
   },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true
+    }
+  }
 };
