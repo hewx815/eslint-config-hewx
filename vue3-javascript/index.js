76e6a8a4d0e3ada71c07be5f8c1d18c49ec089a6
hewx <1825724721@qq.com>
2023-06-17 16:14:46 +0800

    1

---
+++ vue3-javascript/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:promise/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parserOptions: { ecmaVersion: 'latest' },
+  globals: { // 全局变量
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue', 'scss'], // @/引入问题
+      },
+    },
+  },
+  plugins: ['import', 'promise', 'vue'],
+  rules: {
+    'no-param-reassign': ['error', { ignorePropertyModificationsFor: ['state'] }], // 函数参数可修改白名单
+    'max-len': [0], // 忽略每行长度
+  },
+};
