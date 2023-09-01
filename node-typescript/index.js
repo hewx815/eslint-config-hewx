afc2ff02f4eac2987eda1fdfb35f7f63ddaa15b3
hewx815 <1825724721@qq.com>
2023-09-01 09:33:24 +0800

    1.5.2

--- node-typescript/index.js
+++ node-typescript/index.js
@@ -10,39 +10,26 @@ module.exports = {
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:promise/recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+  parser: '@typescript-eslint/parser',
   plugins: ['import', 'promise', 'node', '@typescript-eslint'],
+  parserOptions: {
+    project: true,
+  },
   rules: {
     "max-len": [0],
     "node/no-unsupported-features/es-syntax": [0],
     "node/no-missing-import": [0],
     "import/no-extraneous-dependencies": [0],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-    ]
   },
   settings: {
     "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
-    },
     "import/resolver": {
       "node": {
         "extensions": [".js", ".jsx", ".ts", ".tsx"]
       },
       "typescript": true
-    }
-  },
+    },
+  }
 };
